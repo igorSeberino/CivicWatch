059b647877d7a62f3c4cec7788602bc3aa9cb75c
gabrielcarvallho <gabriellccarvalho.dev@gmail.com>
2025-06-14 00:46:19 -0300

    Adding user session and setting enviroment variables.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var homeRouter = require('./routes/home');
 var favoritosRouter = require('./routes/favoritos');
@@ -22,6 +25,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+
+app.use((req, res, next) => {
+  res.locals.session = req.session || null;
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/home', homeRouter);
